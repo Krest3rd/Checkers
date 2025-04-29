33eecc638d7f4e6a867f52edf331ad834b0280db
Krzysztof Makulewski <knmekowski@gmail.com>
2025-04-29 15:39:47 +0200

    Initial commit (did a lot of stuff)

---
+++ main.c
@@ -0,0 +1,289 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "constants.h"
+#include <allegro5/allegro5.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
+
+// --------------GENERAL--------------
+void must_init(bool test, const char *description)
+{
+    if(test) return;
+
+    printf("couldn't initialize %s\n", description);
+    exit(1);
+}
+
+
+bool collide(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2)
+{
+    if(ax1 > bx2) return false;
+    if(ax2 < bx1) return false;
+    if(ay1 > by2) return false;
+    if(ay2 < by1) return false;
+
+    return true;
+}
+
+int max(int a, int b) {
+    if (a>b) return a;
+    return b;
+}
+
+// ------------DISPLAY----------------
+#define BUFFER_W 640
+#define BUFFER_H 640
+
+#define DISP_SCALE 1
+#define DISP_W (BUFFER_W * DISP_SCALE)
+#define DISP_H (BUFFER_H * DISP_SCALE)
+
+ALLEGRO_DISPLAY* disp;
+ALLEGRO_BITMAP* buffer;
+
+void disp_init()
+{
+    // al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 1, ALLEGRO_SUGGEST);
+    // al_set_new_display_option(ALLEGRO_SAMPLES, 8, ALLEGRO_SUGGEST);
+
+    disp = al_create_display(DISP_W, DISP_H);
+    must_init(disp, "display");
+
+    buffer = al_create_bitmap(BUFFER_W, BUFFER_H);
+    must_init(buffer, "bitmap buffer");
+}
+
+void disp_deinit()
+{
+    al_destroy_bitmap(buffer);
+    al_destroy_display(disp);
+}
+
+void disp_pre_draw()
+{
+    al_set_target_bitmap(buffer);
+}
+
+void disp_post_draw()
+{
+    al_set_target_backbuffer(disp);
+    al_draw_scaled_bitmap(buffer, 0, 0, BUFFER_W, BUFFER_H, 0, 0, DISP_W, DISP_H, 0);
+
+    al_flip_display();
+}
+
+//---------------CHECKERS----------
+
+void initBoard(MAN Board[8][8])
+{
+    for (int i=0;i<3;i++){
+        for (int j=0;j<8;j += 2){
+            if (i==1) {
+                Board[i][j].color = WHITE;
+                Board[i][j].isKing = false;
+
+                Board[7-i][j+1].color=BLACK;
+                Board[7-i][j+1].isKing = false;
+            } else {
+                Board[i][j+1].color = WHITE;
+                Board[i][j+1].isKing = false;
+
+                Board[7-i][j].color=BLACK;
+                Board[7-i][j].isKing = false;
+            }
+        }
+    }
+}
+
+void switchTurn(int* turn){
+    *turn = -1 * (*turn);
+}
+
+
+// Checks if a move is valid
+int isValid(MAN Board[8][8], int Player, int col1, int row1, int col2, int row2) {
+    // Check if not OTB
+    if (col1<0 || col2<0 || row1<0 || row2<0){
+        return false;
+    }
+    if (col1>7 || col2>7 || row1>7 || row2>7){
+        return false;
+    }
+
+    // Check if diffrence between both axes is equal and not grater than 2
+    int xdiff = max(col1-col2,col2-col1);
+    int ydiff = max(row1-row2,row2-row1);
+
+    if (xdiff != ydiff || xdiff>2) return false;
+
+    // Check if theres a MAN on starting pos and no MAN on end pos
+    if (Board[row1][col1].color != Player || Board[row2][col2].color != NONE) return false;
+
+    if (Board[row1][col1].isKing){
+        //Takes down
+        if (row2 == row1 +2) {
+            //Takes left
+            if (col2 == col1 - 2) {
+                if (Board[row1+1][col1-1].color == Player || Board[row1+1][col1-1].color == NONE) return false;
+            }
+            //Takes right
+            if (col2 == col1 + 2) {
+                if (Board[row1+1][col1+1].color == Player || Board[row1+1][col1+1].color == NONE) return false;
+            }
+        } 
+        // Takes up
+        else if (row2 == row1-2){
+            //Takes left
+            if (col2 == col1 - 2) {
+                if (Board[row1-1][col1-1].color == Player || Board[row1-1][col1-1].color == NONE) return false;
+            }
+            //Takes right
+            if (col2 == col1 + 2) {
+                if (Board[row1-1][col1+1].color == Player || Board[row1-1][col1+1].color == NONE) return false;
+            }
+        }
+    } else { // Normal MAN
+        // Check if correct direction (BLACK ^ | WHITE ↓)
+        if (row2 != row1 + Player || row2 != row1 + (2*Player)) return false;
+
+        // Check if takes
+        if (row2 == row1 + (2*Player)) {
+            //Takes left
+            if (col2 == col1 - 2) {
+                if (Board[row1+Player][col1-1].color == Player || Board[row1+Player][col1-1].color == NONE) return false;
+            }
+            //Takes right
+            if (col2 == col1 + 2) {
+                if (Board[row1+Player][col1+1].color == Player || Board[row1+Player][col1+1].color == NONE) return false;
+            }
+        }
+    }
+    return true;
+}
+
+// Move a pawn (no captures)
+int move(MAN Board[8][8], int Player, int col1, int row1, int col2, int row2){
+
+    // Check if move is valid
+    if (isValid(Board, Player, col1, row1, col2, row2)) return false;
+
+    // Move by 1
+    if(col2-col1 != 1) return false;
+
+    // Place on new position 
+    Board[col2][row2].color = Player;
+    if ((row2 == 0 && Player == BLACK) || (row2 == 7 && Player == WHITE)) Board[col2][row2].isKing = true;
+    else Board[row2][col2].isKing = Board[row1][col1].isKing;
+
+    // Remove from old position
+    Board[row1][col1].color = NONE;
+    Board[row1][col1].isKing = false;
+    return 1;
+}  
+
+// Capture a pawn
+int capture(MAN Board[8][8], int Player, int col1, int row1, int col2, int row2){
+    int capped_row = row2 - 1;
+    int capped_col = col2 - 1;
+
+    
+}
+
+//-----------Drawing---------------
+
+void draw_board(MAN board[8][8]){
+    for (int row=0; row<8;row++){
+        for (int col=(row+1)%2; col<8;col+=2){
+            int x = (col)*80;
+            // printf("%d ",x);
+            int y = row*80;
+            al_draw_filled_rectangle(x, y, x+80, y+80, al_map_rgb_f(0, 0, 0));
+            if (board[row][col].color == WHITE){
+                al_draw_filled_circle(x+40,y+40,35,al_map_rgb(200,200,200));
+            }
+            if (board[row][col].color == BLACK){
+                al_draw_filled_circle(x+40,y+40,35,al_map_rgb(55,55,55));
+            }
+        }
+        // printf("\n");
+    };
+}
+
+//----------------MAIN-------------
+int main()
+{
+    must_init(al_init(), "allegro");
+    must_init(al_install_keyboard(), "keyboard");
+
+    ALLEGRO_TIMER* timer = al_create_timer(1.0 / 200.0);
+    must_init(timer, "timer");
+
+    ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
+    must_init(queue, "queue");
+
+    disp_init();
+
+    must_init(al_install_mouse(), "mouse");
+
+    must_init(al_init_primitives_addon(), "primitives");
+
+    al_register_event_source(queue, al_get_keyboard_event_source());
+    al_register_event_source(queue, al_get_mouse_event_source());
+    al_register_event_source(queue, al_get_display_event_source(disp));
+    al_register_event_source(queue, al_get_timer_event_source(timer));
+
+    bool done = false;
+    bool redraw = true;
+    ALLEGRO_EVENT event;
+
+    MAN Board[8][8];
+    initBoard(Board);
+    int turn = BLACK;
+
+    al_start_timer(timer);
+
+    while(1)
+    {
+        al_wait_for_event(queue, &event);
+
+        switch(event.type)
+        {
+            case ALLEGRO_EVENT_TIMER:
+                break;
+
+
+            case ALLEGRO_EVENT_MOUSE_AXES:
+                // cursour.x = event.mouse.x;
+                // cursour.y = event.mouse.y;
+                redraw = true;
+                break;
+            
+            case ALLEGRO_EVENT_MOUSE_BUTTON_DOWN:
+                
+                break;
+
+            case ALLEGRO_EVENT_MOUSE_BUTTON_UP:
+                
+                break;
+
+            case ALLEGRO_EVENT_DISPLAY_CLOSE:
+                done = true;
+                break;
+        }
+
+        if(done)
+            break;
+
+        if(redraw && al_is_event_queue_empty(queue))
+        {
+            disp_pre_draw();
+            al_clear_to_color(al_map_rgb(255,255,255));
+            draw_board(Board);
+            // draw_cursour(cursour.x,cursour.y);
+            disp_post_draw();
+            redraw = false;
+        }
+    }
+
+}
