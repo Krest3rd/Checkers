59a3343d6fe387cdbc3a1a69efa8eafe70bbc7e5
Krzysztof Makulewski <knmekowski@gmail.com>
2025-05-04 12:24:40 +0200

    Forgot to add that

---
+++ constants.h
@@ -0,0 +1,32 @@
+enum COLOR {
+    WHITE = 1, //Idą w dół (row+1)
+    NONE = 0,
+    BLACK = -1 //Idą w górę (row - 1)
+};
+
+enum COLS {
+    A = 0,
+    B,
+    C,
+    D,
+    E,
+    F,
+    G,
+    H
+};
+
+typedef struct MAN {
+    int isKing;
+    int color;
+} MAN;
+
+typedef struct field {
+    int x;
+    int y;
+    MAN pawn;
+} field;
+
+// typedef struct Field {
+//     int isFull;
+//     MAN checker;
+// } Field;
